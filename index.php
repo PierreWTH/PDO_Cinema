6225771dd34fd61ab105a97c00d5d346774649b1
PierreWTH <wietrichpierre@gmail.com>
2023-03-17 14:37:43 +0100

    corrections

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 use Controller\CinemaController;
 
 spl_autoload_register(function ($class_name){
-    include $class_name . 'php';
+    include $class_name . '.php';
 });
 
-$ctrCinema = newController();
+$ctrlCinema = new CinemaController();
 
 if(isset($GET["action"]))
 {
